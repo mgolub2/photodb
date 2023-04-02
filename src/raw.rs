fa3717905e3622597e18bc15f76c10a5c263a9a1
Max Golub <mgolub2@1MAX.local>
2023-04-02 11:22:13 -0700

    Use raw_alloc, no segault because why not...

--- src/raw.rs
+++ src/raw.rs
@@ -16,13 +16,13 @@ impl RawImage {
         match unsafe { libraw_open_buffer(libraw_data, buf.as_ptr() as *const _, buf.len()) } {
             LibRaw_errors_LIBRAW_SUCCESS => match unsafe { libraw_unpack(libraw_data) } {
                 LibRaw_errors_LIBRAW_SUCCESS => {
-                    let raw_image = unsafe { (*libraw_data).rawdata.raw_image };
+                    let raw_image = unsafe { (*libraw_data).rawdata.raw_alloc };
                     let raw_image_size = unsafe {
                         (*libraw_data).sizes.raw_height as usize
                             * (*libraw_data).sizes.raw_width as usize
                     };
                     let raw_image_slice =
-                        unsafe { slice::from_raw_parts(raw_image, raw_image_size) };
+                        unsafe { slice::from_raw_parts(raw_image as *mut u16, raw_image_size) };
                     let mut raw_data = Vec::with_capacity(raw_image_size);
                     raw_data.extend_from_slice(raw_image_slice);
                     unsafe { libraw_close(libraw_data) };
