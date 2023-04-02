2f8c5dde9d9dd384af40c5360dbd02e69c162eec
Max Golub <mgolub2@1MAX.local>
2023-04-02 11:40:46 -0700

    Fix linux build maybe

--- build.rs
+++ build.rs
@@ -1,7 +1,55 @@
 extern crate bindgen;
 
+use std::collections::HashSet;
 use std::env;
 use std::path::PathBuf;
+
+use bindgen::callbacks::{ParseCallbacks, MacroParsingBehavior};
+
+const IGNORE_MACROS
+: [&str; 20] = [
+    "FE_DIVBYZERO",
+    "FE_DOWNWARD",
+    "FE_INEXACT",
+    "FE_INVALID",
+    "FE_OVERFLOW",
+    "FE_TONEAREST",
+    "FE_TOWARDZERO",
+    "FE_UNDERFLOW",
+    "FE_UPWARD",
+    "FP_INFINITE",
+    "FP_INT_DOWNWARD",
+    "FP_INT_TONEAREST",
+    "FP_INT_TONEARESTFROMZERO",
+    "FP_INT_TOWARDZERO",
+    "FP_INT_UPWARD",
+    "FP_NAN",
+    "FP_NORMAL",
+    "FP_SUBNORMAL",
+    "FP_ZERO",
+    "IPPORT_RESERVED",
+];
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> MacroParsingBehavior {
+        if self.0.contains(name) {
+            MacroParsingBehavior::Ignore
+        } else {
+            MacroParsingBehavior::Default
+        }
+    }
+}
+
+impl IgnoreMacros {
+    fn new() -> Self {
+        Self(IGNORE_MACROS
+            .into_iter().map(|s| s.to_owned()).collect())
+    }
+}
+
 
 fn main() {
     // Tell cargo to look for shared libraries in the specified directory
@@ -24,6 +72,9 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(IgnoreMacros::new()))
+        // Longdouble creates warnings about 16-bit numbers, good to ignore if relevant
+        .blocklist_function("strtold")
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
